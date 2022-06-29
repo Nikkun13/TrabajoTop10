fdefebcf401c811ac0c8a59f0273d28c8f9af134
Jesustropro <jdmoraelpro1@gmail.com>
2022-06-29 17:50:41 -0300

    prueba trabajo en equipo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import Header from './components/Header/Header'
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
-import './App.css';
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <p>probando trabajo en equipo, soy jesus</p>
       <Header />
 
       <Main />
 
       <Footer />
-
     </div>
   );
 }
