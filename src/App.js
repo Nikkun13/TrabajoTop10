9a5e5b4186edc51eee9240b4f8a32fdf1bec48f8
Jesustropro <jdmoraelpro1@gmail.com>
2022-07-11 14:23:01 -0300

    whatsapp button v2, fix carrusel

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import PortfolioPage from "./components/PortfolioPage/PortfolioPage";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+  crossorigin="anonymous"
+></script>;
 
 function App() {
   return (
@@ -11,9 +16,14 @@ function App() {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/Portfolio' element={<PortfolioPage />} />
-          <Route path='*' element={<h1 className="error404">Página no encontrada. ERROR 404</h1>} />
+          <Route path="/" element={<Main />} />
+          <Route path="/Portfolio" element={<PortfolioPage />} />
+          <Route
+            path="*"
+            element={
+              <h1 className="error404">Página no encontrada. ERROR 404</h1>
+            }
+          />
         </Routes>
       </BrowserRouter>
       <Footer />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,60 +1,76 @@
-import './Home.css'
-import ReactPlayer from 'react-player'
-import { useState } from 'react'
+import "./Home.css";
+import ReactPlayer from "react-player";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useState } from "react";
 
 const Home = () => {
+  const [playi, setPlay] = useState(true);
 
-    const [playi, setPlay] = useState(true);
+  const [volOnOff, setVolumen] = useState(0);
 
-    const [volOnOff, setVolumen] = useState(0);
+  const playPause = () => {
+    setPlay(!playi);
+  };
 
-    const playPause = () => {
-           setPlay(!playi);
-         };
+  const volumenOnOff = () => {
+    if (volOnOff === 1) {
+      setVolumen(0);
+    } else {
+      setVolumen(1);
+    }
+  };
 
-    const volumenOnOff = () => {
-        if (volOnOff === 1) {
-            setVolumen(0);
-        } else {
-            setVolumen(1);
-        }
-        };
-
-    return (
-        <div id="inicio" className="seccionNoTop">
-            {playi === true ? 
-                <><ReactPlayer
-                url={require('../../media/videos/videoBicicleta.mp4')}
-                width='100%'
-                height='100%' 
-                playing
-                loop
-                volume={`${volOnOff}`}
-                />
-                <div className='areaPlayPause'>
-                    <button className='botonPlayPause' onClick={playPause}>││</button>
-                </div></> : 
-                <><ReactPlayer
-                url={require('../../media/videos/videoBicicleta.mp4')}
-                width='100%'
-                height='100%' 
-                loop
-                volume={`${volOnOff}`}
-                />
-                <div className='areaPlayPause'>
-                    <button className='botonPlayPause' onClick={playPause}>▶</button>
-                </div></>}
-            <div className='areaVolumen'>
-                {volOnOff === 0 ? 
-                    <button className='botonVolumen' onClick={volumenOnOff}>On Volumen</button>:
-                    <button className='botonVolumen' onClick={volumenOnOff}>Off Volumen</button>}
-            </div>
-            {/* video de prueba, luego hay que colocar el video que va */}
-            {/* Me volvi loco porque no fucionaba bien el autoplay, buscando buscando encontre el porque...
+  return (
+    <div id="inicio" className="seccionNoTop">
+      {playi === true ? (
+        <>
+          <ReactPlayer
+            url={require("../../media/videos/videoBicicleta.mp4")}
+            width="100%"
+            height="100%"
+            playing
+            loop
+            volume={`${volOnOff}`}
+          />
+          <div className="areaPlayPause">
+            <button className="botonPlayPause" onClick={playPause}>
+              ││
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <ReactPlayer
+            url={require("../../media/videos/videoBicicleta.mp4")}
+            width="100%"
+            height="100%"
+            loop
+            volume={`${volOnOff}`}
+          />
+          <div className="areaPlayPause">
+            <button className="botonPlayPause" onClick={playPause}>
+              ▶
+            </button>
+          </div>
+        </>
+      )}
+      <div className="areaVolumen">
+        {volOnOff === 0 ? (
+          <button className="botonVolumen" onClick={volumenOnOff}>
+            On Volumen
+          </button>
+        ) : (
+          <button className="botonVolumen" onClick={volumenOnOff}>
+            Off Volumen
+          </button>
+        )}
+      </div>
+      {/* video de prueba, luego hay que colocar el video que va */}
+      {/* Me volvi loco porque no fucionaba bien el autoplay, buscando buscando encontre el porque...
             Chrome desactiva el autoplay de los videos que tienen volumen, probe poniendo el volumen en 0 y
             funciona bien. Asi que si el video de Fran tiene volumen, no puede tener autoplay, una u otra. */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
