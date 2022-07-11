a3cdd32ed15741015e9ac8a7590a5867bea48c3d
Nicolas Arcieri <nicolasarcieri@gmail.com>
2022-07-11 12:55:32 -0300

    Nico-ContactV0.5

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,25 @@ const Contact = () => {
 
     return (
         <div id="contacto"  className="seccion">
-            <h1>ESCRIBENOS</h1>
+            <h4 className="contactTituloUno">¿Tienes un proyecto o idea en mente que necesites ayuda?</h4>
+            <h2 className="contactTituloDos">¡Escríbenos!</h2>
+            <h5 className="contactMail">ortizpellegrinifrancisco@gmail.com</h5>
+            <div className="contenedorFormulario">
+                <p className="contForm">Contacto</p>
+                <form action="/my-handling-form-page" method="post">
+                    <ul>
+                        <li>
+                            <input className="nombreForm" type="text" id="name" name="user_name"/>
+                        </li>
+                        <li>
+                            <input className="mailForm" type="email" id="mail" name="user_mail"/>
+                        </li>
+                        <li>
+                            <textarea className="textForm" id="msg" name="user_message"></textarea>
+                        </li>
+                    </ul>
+                </form>
+            </div>
         </div>
     )
 }
