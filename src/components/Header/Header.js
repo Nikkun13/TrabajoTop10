811ba52f9977ed4370f82a2c876dbdee0ccfd2aa
Jesustropro <jdmoraelpro1@gmail.com>
2022-07-21 16:43:16 -0300

    copy alerts & button v4

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import Navbar from "../Navbar/Navbar";
 // import MenuButton from "../MenuButton/MenuButton";
 import "./Header.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useState } from "react";
 
 const Header = () => {
   // const [open, setOpen] = useState(false);
@@ -11,15 +12,25 @@ const Header = () => {
   // const handleClick = () => {
   //   setOpen(!open);
   // };
-
+  const [alert, setAlert] = useState(0);
+  const alertCopy = () => {
+    setAlert(1);
+    setTimeout(() => {
+      setAlert(0);
+    }, 2000);
+  };
   return (
     <div className="header">
       <div className="contenedorHeader">
         <Navbar />
         <CopyToClipboard text={`${window.location.origin}`}>
-          <p className="compartir">COMPARTIR</p>
+          <p className="compartir" onClick={alertCopy}>
+            COMPARTIR
+          </p>
         </CopyToClipboard>
-
+        <button className="copiado" style={{ opacity: `${alert}` }}>
+          ¡link copiado!
+        </button>
         {/* <NavbarBurger open={open} />
         <MenuButton open={open} handleClick={handleClick} /> */}
       </div>
